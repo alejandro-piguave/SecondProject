770b722daacc1eba314bdd20a6444b6926562089
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2019-10-02 18:10:25 +0200

    Initial Commit

---
+++ Second project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Second project
+//
+//  Created by Dev2 on 02/10/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
