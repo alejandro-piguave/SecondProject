00454e05a66480e477667ab12e56f8ea28f424dd
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2019-10-16 20:07:54 +0200

    Added 2 detail views

---
+++ Second project/UI/Views/FirstTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstTableViewCell.swift
+//  Second project
+//
+//  Created by Dev2 on 16/10/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
